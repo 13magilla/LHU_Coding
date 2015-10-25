997c6e601585148b2653c3ef770e5a89a20ebaa8
worky68 <drew.workman@gmail.com>
2015-10-24 20:37:19 -0400

    Added login index page and css

--- login/php/login.php
+++ login/php/login.php
@@ -1,7 +1,8 @@
 <?php
+// TODO: Take this and construct authenticator class
 
 $command = "ypcat passwd | grep -w -i ^".escapeshellarg(urldecode($_POST['UN']));
-$result = shell_exec($command);
+$result = shell_exec($command); // execute shell command
 
 if($result == NULL) {	
 
@@ -10,20 +11,22 @@ if($result == NULL) {
 
 else {
 	
-	$userinfo = explode(":", $result);// split the result by : seporators
-	$storedpwhash = $userinfo[1];// set the pw info
-	$storedusername = $userinfo[0];
+	$userInfo = explode(":", $result);// split the result by : separators
+    $storedUsername = $userinfo[0];
+	$storedHash = $userinfo[1];// set the pw hash info
 	
-
+    // sanity check on username and check on hashed password against stored hash password
 	if(urldecode($_POST['UN']) == $storedusername && $storedpwhash == crypt(urldecode($_POST['PW']), $storedpwhash)) {
 		
-		header('Location: ../../home/index.html');
+		header('Location: ../../home/index.html'); // success redirect to the home page
+        
+        //TODO create session
 	
 	}
 
 	else {
 	
-		header('Location: ../index.html');
+		header('Location: ../index.html'); // failure redirect to the login page
 
 	}
 	
